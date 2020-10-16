0732291936e04fb587667115afecb1710814be5e
NaniDeveloper <60369403+NaniDeveloper@users.noreply.github.com>
2020-10-16 15:11:59 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/7o3v6p3xyoq8nj0/advanced_model.pkl?raw=1'
+export_file_name = 'advanced_model.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Prediction - High Grade Glioma', 'Prediction - Low Grade Glioma', 'Prediction - None']
 path = Path(__file__).parent
 
 app = Starlette()
